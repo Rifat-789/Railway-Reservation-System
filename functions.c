1518d9ed3287483cef137af45546a3586e61a58a
Md. Rifat <jahidulislamrefat6@gmail.com>
2026-05-10 20:43:31 +0600

    Add loadUser and loadAdmin function

--- functions.c
+++ functions.c
@@ -17,15 +17,36 @@ void loadTrains(Train trains[], int *countTrain){
     fread(trains, sizeof(Train), *countTrain, pfile);
 
     fclose(pfile);
-
 }
 
 void loadUsers(User users[], int *countUser){
+    FILE *pfile = fopen("train.dat", "rb");
 
+    if(pfile == NULL){
+        *countUser = 0;
+        fclose(pfile);
+        return;
+    }
+
+    fread(countUser, sizeof(int), 1, pfile);
+    fread(users, sizeof(Train), *countUser, pfile);
+
+    fclose(pfile);
 }
 
 void loadAdmins(Admin admins[], int *countAdmin){
+    FILE *pfile = fopen("train.dat", "rb");
 
+    if(pfile == NULL){
+        *countAdmin = 0;
+        fclose(pfile);
+        return;
+    }
+
+    fread(countAdmin, sizeof(int), 1, pfile);
+    fread(admins, sizeof(Train), *countAdmin, pfile);
+
+    fclose(pfile);
 }
 
 void printTitle(char title[], int padding){
